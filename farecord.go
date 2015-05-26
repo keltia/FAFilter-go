d4a04af80a70fcaace52944dd63ac2530bd87f59
Ollivier Robert <roberto@keltia.net>
2015-05-26 22:14:00 +0200

    Record timestamps.

--- farecord.go
+++ farecord.go
@@ -19,6 +19,33 @@ func (line *FArecord) checkRecord() bool {
 	var cont		bool
 	var myTimestamp time.Time
 
+	// Record timestamps
+	if line.Clock != "" {
+		var value int64
+
+		if _, err := fmt.Sscanf(line.Clock, "%d", &value); err != nil {
+			myTimestamp = time.Unix(value, 0)
+		}
+		// gather stats
+		if (timeStats.FirstSeen == time.Time{}) {
+			timeStats.FirstSeen = myTimestamp
+		}
+		timeStats.LastSeen = myTimestamp
+
+		if (timeStats.Lowest == time.Time{}) {
+			timeStats.Lowest = myTimestamp
+			timeStats.Highest = myTimestamp
+		} else {
+			// check lowest/highest
+			if myTimestamp.Before(timeStats.Lowest) {
+				timeStats.Lowest = myTimestamp
+			}
+			if myTimestamp.After(timeStats.Highest) {
+				timeStats.Highest = myTimestamp
+			}
+		}
+	}
+
 	// Check for -a
 	if fAircraftId != "" {
 		cont = checkRegex(line.Ident, rAircraftId)
