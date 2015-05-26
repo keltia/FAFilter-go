b1801ea86a0d42d1b38c67316c28be0a80103327
Ollivier Robert <roberto@keltia.net>
2015-05-26 22:15:42 +0200

    We are supposed to have only "position" updates but wrap anyway.

--- farecord.go
+++ farecord.go
@@ -19,65 +19,8 @@ func (line *FArecord) checkRecord() bool {
 	var cont		bool
 	var myTimestamp time.Time
 
-	// Record timestamps
-	if line.Clock != "" {
-		var value int64
-
-		if _, err := fmt.Sscanf(line.Clock, "%d", &value); err != nil {
-			myTimestamp = time.Unix(value, 0)
-		}
-		// gather stats
-		if (timeStats.FirstSeen == time.Time{}) {
-			timeStats.FirstSeen = myTimestamp
-		}
-		timeStats.LastSeen = myTimestamp
-
-		if (timeStats.Lowest == time.Time{}) {
-			timeStats.Lowest = myTimestamp
-			timeStats.Highest = myTimestamp
-		} else {
-			// check lowest/highest
-			if myTimestamp.Before(timeStats.Lowest) {
-				timeStats.Lowest = myTimestamp
-			}
-			if myTimestamp.After(timeStats.Highest) {
-				timeStats.Highest = myTimestamp
-			}
-		}
-	}
-
-	// Check for -a
-	if fAircraftId != "" {
-		cont = checkRegex(line.Ident, rAircraftId)
-		if cont == false {
-			recordStats.SkippedAircraftId++
-			return cont
-		}
-	}
-
-	// Check for -x
-	if fHexid != "" {
-		cont = checkRegex(line.Hexid, rHexid)
-		if cont == false {
-			recordStats.SkippedHexid++
-			return cont
-		}
-	}
-
-	// Check for -t
-	if fUpdateType != "" {
-		cont = checkRegex(line.UpdateType, rUpdateType)
-		if cont == false {
-			recordStats.SkippedUpdateType++
-			return false
-		}
-	}
-
-
-	// fallthrough
-	return true
-
 	if line.Type == "position" {
+		// Record timestamps
 		if line.Clock != "" {
 			var value int64
 
@@ -102,10 +45,38 @@ func (line *FArecord) checkRecord() bool {
 					timeStats.Highest = myTimestamp
 				}
 			}
-
-
 		}
-	}
+
+		// Check for -a
+		if fAircraftId != "" {
+			cont = checkRegex(line.Ident, rAircraftId)
+			if cont == false {
+				recordStats.SkippedAircraftId++
+				return cont
+			}
+		}
+
+		// Check for -x
+		if fHexid != "" {
+			cont = checkRegex(line.Hexid, rHexid)
+			if cont == false {
+				recordStats.SkippedHexid++
+				return cont
+			}
+		}
+
+		// Check for -t
+		if fUpdateType != "" {
+			cont = checkRegex(line.UpdateType, rUpdateType)
+			if cont == false {
+				recordStats.SkippedUpdateType++
+				return false
+			}
+		}
+
+	} // position
+
+	// fallthrough
 	return true
 }
 
