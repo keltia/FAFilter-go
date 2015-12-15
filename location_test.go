bae5fddc2adc68e361df279f61135e6ce8dff2e6
Ollivier Robert <roberto@keltia.net>
2015-12-15 17:35:05 +0100

    Add tests for pointInPolygon().

--- location_test.go
+++ location_test.go
@@ -11,7 +11,6 @@ var (
 		{44.2924010852901, -6.943359375},
 		{41.6236553906864, 13.798828125},
 		{49.6818468994013, 9.0087890625},
-		{49.6818468994013, 9.0087890625},
 		{52.3890110622346, 3.2958984375},
 	}}
 
@@ -21,7 +20,6 @@ var (
 		{48.2978124924372, -5.625},
 		{42.7954006530372, 1.669921875},
 		{42.8954006530372, 3.669921875},
-		{47.8574028946582, 8.701171875},
 		{47.8574028946582, 8.701171875},
 	}}
 )
@@ -45,5 +43,17 @@ func TestPolygonLen(t *testing.T) {
 	mylen = data2.len()
 	if mylen != len(data2.P) {
 		t.Errorf("Error loading polygon %v, invalid length", data2, mylen)
+	}
+}
+
+func TestPointInPolygon(t *testing.T) {
+	good := Location{45,5}
+	if !good.pointInPolygon(data1.P) {
+		t.Errorf("Point %v should be in polygon %v", good, data1.P)
+	}
+
+	bad := Location{50,-5}
+	if bad.pointInPolygon(data1.P) {
+		t.Errorf("Point %v should not be in polygon %v", bad, data1.P)
 	}
 }
